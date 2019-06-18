bde361db505ebb1fb8c73bfc220a0decbc7ce424
Dima <tinchodimaria@gmail.com>
2019-06-18 20:33:51 -0300

    modificacion hecha en la facultad

--- Sopa de letras/SopaDeLetras.py
+++ Sopa de letras/SopaDeLetras.py
@@ -1,18 +1,18 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+#sys.version_info[0] saber version del pysimplegui
+
 import random
 import string
 import sys
-if sys.version_info[0] >= 3:
-    import PySimpleGUI as sg
-else:
-    import PySimpleGUI27 as sg    
+import PySimpleGUI as sg
 
-#sys.version_info[0] saber version del pysimplegui
 
 BOX_SIZE = 22
 
 listaPalabras=['Palabra1', 'Palabra2', 'Palabra3','Palabra4', 'Palabra5', 'Palabra6']
 layout = [
-    [sg.Text('SOPA DE LETRAS by PySimpleGUI', font=(15)), sg.Text('                                                                                      Lista de Palabras:',font=(15))],
+    [sg.Text('SOPA DE LETRAS by PySimpleGUI', font=(15)), sg.Text('                                                                  Lista de Palabras:',font=(15))],
     [sg.Graph((600, 550), (0, 450), (450, 0), key='Graph', change_submits=True, drag_submits=False), sg.Listbox(values=listaPalabras, size=(16, 30))],
     [sg.Button('Buscar Palabra'), sg.Button('Ayuda'), sg.Button('Exit')]
 ]
@@ -28,9 +28,9 @@ num=random.randrange(3)
 
 
 matriz = []
-for i in range(20):
+for i in range(10):
     matriz.append([])
-    for j in range(20):  
+    for j in range(10):  
         letra = random.choice(string.ascii_uppercase)
         g.DrawText('{}'.format(letra), (i * BOX_SIZE + 12, j * BOX_SIZE + 12)) 
         matriz[i].append(letra)
@@ -49,12 +49,17 @@ while True:
         y = mouse[0]//BOX_SIZE
         x = mouse[1]//BOX_SIZE
         print('x:',x,'y:',y)
-        g.DrawRectangle((y * BOX_SIZE, x * BOX_SIZE), (y * BOX_SIZE+BOX_SIZE-2, x * BOX_SIZE+BOX_SIZE-2), line_color='blue')
-        palabra.append(matriz[x][y])
-
+        g.DrawRectangle((y * BOX_SIZE, x * BOX_SIZE), (y * BOX_SIZE+BOX_SIZE-2, x * BOX_SIZE+BOX_SIZE-2), line_color='none') 
+        try:    
+            palabra.append(matriz[y][x])
+            g.DrawRectangle((y * BOX_SIZE, x * BOX_SIZE), (y * BOX_SIZE+BOX_SIZE-2, x * BOX_SIZE+BOX_SIZE-2), line_color='blue')  
+        except(IndexError): 
+            print('fuera de rango')
+            
     if event == 'Buscar Palabra':
         for element in palabra:
             print(element)
+        palabra=[]           
 
 window.Close() 
       
