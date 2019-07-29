cf832924a0b5ff3d04b4265d2c08560b2379e3e0
bgomez97 <bag97ve@gmail.com>
2019-07-29 03:11:24 -0300

    Se paso el codigo de ventanaConfiguracion a POO

--- digom/Digom_Main.py
+++ digom/Digom_Main.py
@@ -1,6 +1,10 @@
 from SopaDeLetras import Digom
+import ventanaConfiguracion
+from Configuracion import Configuracion
 
 if __name__ == "__main__":
+    conf = Configuracion()
+    conf.graficarConfiguracion()
     d = Digom()
     d.crearMatriz()
     d.meterPalabrasEnMatriz()
